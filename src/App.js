0d5603b6fb3e239304d745eb739869ba3ad472cd
Thomas Weld <thomas.weld@gmail.com>
2019-07-20 23:09:16 -0400

    initial setup for github oauth and some customization with new logo and link to login - successfully getting github oauth code in URL, next step is to exchange this code for access token

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import octocat from "./Octocat.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={octocat} className="App-logo" alt="logo" />
+        <p>Login with GitHub to get started.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={`https://github.com/login/oauth/authorize?client_id=${
+            process.env.REACT_APP_GITHUB_CLIENT_ID
+          }&scope=public_repo`}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Login
         </a>
       </header>
     </div>
