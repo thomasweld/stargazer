4e71ca7c3defdcbc6a76074a96f35af01e169680
Thomas Weld <thomas.weld@gmail.com>
2019-07-21 00:24:50 -0400

    stuck on the step of getting client access_token - POST https://github.com/login/oauth/access_token

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from "react";
 import octocat from "./Octocat.png";
 import "./App.css";
+import axios from "axios";
+
+const getAccessToken = async () => {
+  try {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+    let accessToken;
+    if (code) {
+      accessToken = await axios.post(
+        `http://localhost:9999/authenticate/?code=${code}`
+        // http://stargazer-gatekeeper.herokuapp.com/authenticate
+        // instance of https://github.com/prose/gatekeeper
+        // {},
+        // { headers: { "Access-Control-Allow-Origin": "*" } }
+      );
+      console.log(accessToken);
+    }
+
+    console.log("getAccessToken", accessToken);
+    return accessToken;
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 function App() {
+  getAccessToken();
+
   return (
     <div className="App">
       <header className="App-header">
