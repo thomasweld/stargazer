91723f8c9fb0bdf48b025f67a8c8dc06a572a3b2
Thomas Weld <thomas.weld@gmail.com>
2019-07-22 21:54:30 -0400

    success with task - enables a user to view repositories they have starred - added pagination and some minimal ui work

--- src/components/UserStarredRepos.js
+++ src/components/UserStarredRepos.js
@@ -1,24 +1,23 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { Query } from "react-apollo";
+import get from "lodash.get";
 
 const UserStarredRepos = ({ cursor }) => {
   const GET_USER_STARRED_REPOS = gql`
-    {
+    query($cursor: String) {
       viewer {
         login
         name
-        starredRepositories(first: 10, after: ${cursor}) {
+        starredRepositories(last: 10, before: $cursor) {
+          pageInfo {
+            hasPreviousPage
+            startCursor
+          }
           edges {
-            cursor
             node {
               id
               name
-              primaryLanguage {
-                id
-                name
-                color
-              }
             }
           }
         }
@@ -26,23 +25,65 @@ const UserStarredRepos = ({ cursor }) => {
     }
   `;
   return (
-    <Query query={GET_USER_STARRED_REPOS}>
-      {({ loading, error, data }) => {
+    <Query
+      query={GET_USER_STARRED_REPOS}
+      variables={{
+        cursor: null
+      }}
+    >
+      {({ data, error, loading, fetchMore }) => {
         if (loading) return <div>Loading...</div>;
         if (error) return <div>Error :(</div>;
-        if (
-          data &&
-          data.viewer &&
-          data.viewer.starredRepositories &&
-          data.viewer.starredRepositories.edges &&
-          data.viewer.starredRepositories.edges.length
-        ) {
-          console.log(data);
-          return data.viewer.starredRepositories.edges.map(edge => (
-            <p style={{ fontWeight: "bold" }} key={edge.node.id}>
-              ☆ {edge.node.name}
-            </p>
-          ));
+        if (get(data, "viewer.starredRepositories.edges.length")) {
+          return (
+            <React.Fragment>
+              <h4>My Starred Repos:</h4>
+              {data.viewer.starredRepositories.edges.map(edge => (
+                <p style={{ fontWeight: "bold" }} key={edge.node.id}>
+                  ☆ {edge.node.name}
+                </p>
+              ))}
+              {get(
+                data,
+                "viewer.starredRepositories.pageInfo.hasPreviousPage"
+              ) && (
+                <button
+                  onClick={() =>
+                    fetchMore({
+                      variables: {
+                        cursor: get(
+                          data,
+                          "viewer.starredRepositories.pageInfo.startCursor"
+                        )
+                      },
+                      updateQuery: (prev, { fetchMoreResult }) => {
+                        if (!fetchMoreResult) return prev;
+                        return {
+                          ...prev,
+                          viewer: {
+                            ...prev.viewer,
+                            starredRepositories: {
+                              ...prev.viewer.starredRepositories,
+                              pageInfo:
+                                fetchMoreResult.viewer.starredRepositories
+                                  .pageInfo,
+                              edges: [
+                                ...prev.viewer.starredRepositories.edges,
+                                ...fetchMoreResult.viewer.starredRepositories
+                                  .edges
+                              ]
+                            }
+                          }
+                        };
+                      }
+                    })
+                  }
+                >
+                  Load More...
+                </button>
+              )}
+            </React.Fragment>
+          );
         }
         return null;
       }}
